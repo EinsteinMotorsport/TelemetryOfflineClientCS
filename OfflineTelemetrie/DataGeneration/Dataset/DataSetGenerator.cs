c34dfada855f4988be23388679e9ffe47a1db3ad
Alex Stenke <alex.stenke@einstein-motorsport.com>
2018-11-22 22:10:54 +0100

    Added NLog Config

--- OfflineTelemetrie/DataGeneration/Dataset/DataSetGenerator.cs
+++ OfflineTelemetrie/DataGeneration/Dataset/DataSetGenerator.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.IO;
+using NLog;
 
 namespace OfflineTelemetrie.DataGeneration.Dataset
 {
     class DataSetGenerator
     {
+        private static Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
         private FileStream fileStream;
         private StreamReader reader;
         private string datasetConfigFilePath;
-        private ObservableCollection<Dataset> datasets;
 
         private ObservableCollection<Dataset> dataset;
         public ObservableCollection<Dataset> Dataset
@@ -22,31 +24,49 @@ namespace OfflineTelemetrie.DataGeneration.Dataset
         {
             this.datasetConfigFilePath = "..\\..\\..\\..\\_Configs\\DatasetConfig.txt";
 
-            this.fileStream = new FileStream(datasetConfigFilePath, FileMode.Open, FileAccess.Read);
-            this.reader = new StreamReader(fileStream);
-            this.datasets = new ObservableCollection<Dataset>();
+            try
+            {
+                this.fileStream = new FileStream(datasetConfigFilePath, FileMode.Open, FileAccess.Read);
+                this.reader = new StreamReader(fileStream);
+            }
+            catch (Exception e)
+            {
+                logger.Error("Couldn't open DatasetConfig.txt file");
+                logger.Error(e.Message);
+            }
+
+
+            this.Dataset = new ObservableCollection<Dataset>();
         }
 
         public ObservableCollection<Dataset> ReadDatasetConfigFile()
         {
-            while (!reader.EndOfStream)
+            try
             {
-                var line = reader.ReadLine();
-
-                if (!string.IsNullOrEmpty(line))
+                while (!reader.EndOfStream)
                 {
-                    var id = Convert.ToInt32(line.Substring(0, 3));
-                    var lengthName = line.LastIndexOf('~') - 4;
-                    var name = line.Substring(4, lengthName);
-                    var unit = line.Substring(5 + lengthName, line.Length - 5 - lengthName);
+                    var line = reader.ReadLine();
 
-                    Dataset.Add(new Dataset(id, name, unit));
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        var id = Convert.ToInt32(line.Substring(0, 3));
+                        var lengthName = line.LastIndexOf('~') - 4;
+                        var name = line.Substring(4, lengthName);
+                        var unit = line.Substring(5 + lengthName, line.Length - 5 - lengthName);
+
+                        Dataset.Add(new Dataset(id, name, unit));
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                logger.Error("Couldn't read DatasetConfig.txt file");
+                logger.Error(e.Message);
+            }
+
+
 
             return Dataset;
         }
-
-
     }
 }
